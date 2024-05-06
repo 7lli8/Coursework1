5817f3d00829de16bf25119cfd995cfc7e48b2da
7lli8 <bachurina.yulia1337@gmail.com>
2024-05-06 23:23:26 +0500

    add upload_file2 and login

--- files/urls.py
+++ files/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import hello, upload_file, read_file
+from .views import hello, upload_file, read_file, upload_file2
 
 urlpatterns = [
     path("", hello),
     path("upload/", upload_file),
     path("read/", read_file),
+    path("upload2/", upload_file2)
 ]
