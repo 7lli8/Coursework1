d870e3583d6fad2c61656e9f0dd3774386107a4d
7lli8 <bachurina.yulia1337@gmail.com>
2024-05-18 17:10:37 +0500

    add templates

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 
-from .views import signup_view, signin_view, logout_view
+from .views import (
+    signup_view,
+    signin_view,
+    logout_view,
+    account_view,
+    contacts_view,
+    help_view,
+)
 
 urlpatterns = [
     path("signup/", signup_view, name="signup"),
     path("signin/", signin_view, name="signin"),
-    path("logout", logout_view, name="logout"),
+    path("logout/", logout_view, name="logout"),
+    path("account/", account_view, name="account"),
+    path("contacts/", contacts_view, name="contacts"),
+    path("help/", help_view, name="help"),
 ]
