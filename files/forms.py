a0b4a80c657fa50f02322ed280f664cf6384029a
7lli8 <bachurina.yulia1337@gmail.com>
2024-06-11 22:05:38 +0500

    update data

--- files/forms.py
+++ files/forms.py
@@ -2,12 +2,13 @@ from django import forms
 
 
 class FileUploadForm(forms.Form):
-    file = forms.FileField(widget=forms.FileInput(attrs={'accept': 'application/dicom'}))
+    file = forms.FileField(
+        widget=forms.FileInput(attrs={"accept": "application/dicom"})
+    )
 
     # file = forms.FileField(label = "Файл")
     def clean_file(self):
-        file = self.cleaned_data.get('file')
+        file = self.cleaned_data.get("file")
         if not file:
-            raise forms.ValidationError('Нужно выбрать файл для загрузки')
+            raise forms.ValidationError("Нужно выбрать файл для загрузки")
         return file
-
