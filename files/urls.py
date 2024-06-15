5e949b76b003cfcfffc7e70f7ddffd0705f497bf
7lli8 <bachurina.yulia1337@gmail.com>
2024-06-15 16:32:45 +0500

    add templates: signin, signup, contacts, account

--- files/urls.py
+++ files/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from . import views
-from .views import home_view, upload_file, read_file, upload_file2, hello, display_file
+from .views import home_view, read_file, upload_file2, hello, display_file
 
 urlpatterns = [
     path("", home_view, name="home"),
-    path("upload/", upload_file),
+    # path("upload/", upload_file),
     path("read/", read_file),
-    path("upload2/", upload_file2),
+    path("upload/", upload_file2),
     path("index/", hello),
-    path('display/', views.display_file, name='display_file'),  # URL отображения файла
+    path("display/", views.display_file, name="display_file"),  # URL отображения файла
 ]
--- files/views.py
+++ files/views.py
@@ -12,11 +12,11 @@ def hello(request):
     return render(request, "files/index.html")
 
 
-def upload_file(request):
-    if request.method == "GET":
-        return HttpResponse("123")
-    else:
-        pass
+# def upload_file(request):
+#     if request.method == "GET":
+#         return HttpResponse("123")
+#     else:
+#         pass
 
 
 def read_file(request):
@@ -28,6 +28,8 @@ def read_file(request):
         "files/read.html",
         {"props": [f"{key}: {str(getattr(file, key))}" for key in keys]},
     )
+
+
 def home_view(request):
     return render(request, "files/home.html")
 
@@ -35,23 +37,34 @@ def home_view(request):
 # Функция для загрузки файла (только для авторизованных пользователей)
 def upload_file2(request):
     if request.method == "POST":  # Проверяем, был ли отправлен POST запрос
-        form = FileUploadForm(request.POST, request.FILES)  # Инициализируем форму загрузки файлов с данными из запроса
+        form = FileUploadForm(
+            request.POST, request.FILES
+        )  # Инициализируем форму загрузки файлов с данными из запроса
         if form.is_valid():  # Проверяем валидность данных формы
-            file = request.FILES['file']  # Получаем загруженный файл из формы
-            uploaded_by = request.user if request.user.is_authenticated else None  # Определяем пользователя, загрузившего файл (если он авторизован)
-            fp = CustomFiles(file=form.cleaned_data["file"],
-                             file_name=file.name,
-                             uploaded_by=uploaded_by)  # Создаем экземпляр CustomFiles для сохранения файла в базе данных
+            file = request.FILES["file"]  # Получаем загруженный файл из формы
+            uploaded_by = (
+                request.user if request.user.is_authenticated else None
+            )  # Определяем пользователя, загрузившего файл (если он авторизован)
+            fp = CustomFiles(
+                file=form.cleaned_data["file"],
+                file_name=file.name,
+                uploaded_by=uploaded_by,
+            )  # Создаем экземпляр CustomFiles для сохранения файла в базе данных
             fp.save()  # Сохраняем файл в базе данных
 
             # После успешной загрузки, подготовка данных для отображения DICOM
             image_base64, tags_html = display_dicom_file(fp.file.path)
 
             # Перенаправление на страницу отображения DICOM
-            return render(request, "files/display.html",
-                                {"image_base64": image_base64, "tags_html": tags_html})
+            return render(
+                request,
+                "files/display.html",
+                {"image_base64": image_base64, "tags_html": tags_html},
+            )
     else:
-        form = FileUploadForm()  # Если метод запроса не POST, создаем пустую форму загрузки файлов
+        form = (
+            FileUploadForm()
+        )  # Если метод запроса не POST, создаем пустую форму загрузки файлов
 
     # Рендеринг страницы загрузки файла с формой
     return render(request, "files/upload_file.html", {"form": form})
@@ -60,18 +73,21 @@ def upload_file2(request):
 def display_file(request):
     try:
         # Получение последнего загруженного файла текущего пользователя
-        last_uploaded_file = CustomFiles.objects.filter(uploaded_by=request.user).latest('uploaded_at')
+        last_uploaded_file = CustomFiles.objects.filter(
+            uploaded_by=request.user
+        ).latest("uploaded_at")
         file_path = last_uploaded_file.file.path  # Получение пути к файлу
-        image_base64, tags_html = display_dicom_file(file_path)  # Получение изображения и данных DICOM для отображения
+        image_base64, tags_html = display_dicom_file(
+            file_path
+        )  # Получение изображения и данных DICOM для отображения
 
         # Рендеринг страницы отображения DICOM файла
-        return render(request, "files/display.html", {"image_base64": image_base64, "tags_html": tags_html})
+        return render(
+            request,
+            "files/display.html",
+            {"image_base64": image_base64, "tags_html": tags_html},
+        )
     except CustomFiles.DoesNotExist:
         # В случае отсутствия загруженных файлов пользователем, возвращаем сообщение об ошибке и перенаправляем на страницу загрузки файла
         messages.error(request, "Файл не найден или у вас нет загруженных файлов")
         return redirect("upload_file2")
-
-
-
-
-
